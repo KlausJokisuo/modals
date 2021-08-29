00aef25bdf745c69997877fd3dc042b672e2bd32
Klaus <36441457+klausjokisuo@users.noreply.github.com>
2021-08-29 18:15:09 +0300

    0.0.1

---
+++ test/modals_positioned_test.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:modals/modals.dart';
+
+void main() {
+  group('positioned', () {
+    testWidgets('position modal with ModalEntry.positioned', (tester) async {
+      const right = 190.0;
+      const bottom = 155.0;
+      const childrenHeight = 40.0;
+      const childrenWidth = 40.0;
+
+      await tester.pumpWidget(const MaterialApp(
+          home: TestWidget(
+              right: right,
+              bottom: bottom,
+              childrenHeight: childrenHeight,
+              childrenWidth: childrenWidth)));
+      await tester.pump();
+      final modalBackgroundSize =
+          tester.getRect(find.byKey(const Key('modal'))).size;
+
+      final modalChildrenPosition =
+          tester.getCenter(find.byKey(const Key('modalChildren')));
+
+      final modalChildrenSize =
+          tester.getRect(find.byKey(const Key('modalChildren'))).size;
+
+      final exceptedDx = modalBackgroundSize.width -
+          (modalChildrenPosition.dx + (modalChildrenSize.width / 2));
+
+      final exceptedDy = modalBackgroundSize.height -
+          (modalChildrenPosition.dy + (modalChildrenSize.height / 2));
+
+      expect(Offset(exceptedDx, exceptedDy), const Offset(right, bottom));
+    });
+  });
+}
+
+class TestWidget extends StatefulWidget {
+  const TestWidget(
+      {Key? key,
+      required this.right,
+      required this.bottom,
+      required this.childrenWidth,
+      required this.childrenHeight})
+      : super(key: key);
+
+  final double right;
+  final double bottom;
+
+  final double childrenWidth;
+  final double childrenHeight;
+
+  @override
+  _TestWidgetState createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  @override
+  void initState() {
+    super.initState();
+    showModal(ModalEntry.positioned(
+      context,
+      key: const Key('modal'),
+      tag: 'modalTag',
+      right: widget.right,
+      bottom: widget.bottom,
+      child: Container(
+        key: const Key('modalChildren'),
+        color: Colors.yellow,
+        width: widget.childrenWidth,
+        height: widget.childrenHeight,
+      ),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
---
+++ test/modals_show_positioned_test.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:modals/modals.dart';
+
+void main() {
+  group('show positioned', () {
+    testWidgets('show modal using initState', (tester) async {
+      await tester.pumpWidget(const MaterialApp(home: TestWidget()));
+      await tester.pump();
+      expect(find.byKey(const Key('firstModal')), findsOneWidget);
+    });
+
+    testWidgets('show modal using gesture', (tester) async {
+      await tester.pumpWidget(const MaterialApp(home: TestWidget()));
+      await tester.tap(find.byKey(
+        const Key('secondModalButton'),
+      ));
+      await tester.pumpAndSettle();
+      expect(find.byKey(const Key('secondModal')), findsOneWidget);
+    });
+  });
+}
+
+class TestWidget extends StatefulWidget {
+  const TestWidget({Key? key}) : super(key: key);
+
+  @override
+  _TestWidgetState createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  @override
+  void initState() {
+    super.initState();
+    showModal(ModalEntry.positioned(context,
+        key: const Key('firstModal'),
+        tag: 'firstModal',
+        left: 200,
+        top: 200,
+        child: const SizedBox.shrink()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      key: const Key('secondModalButton'),
+      onTap: () {
+        showModal(ModalEntry.positioned(context,
+            key: const Key('secondModal'),
+            tag: 'secondModal',
+            left: 200,
+            top: 200,
+            child: const SizedBox.shrink()));
+      },
+      child: Container(
+        color: Colors.red,
+        height: 50,
+        width: 50,
+      ),
+    );
+  }
+}
